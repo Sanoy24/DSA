70cefc3fa2e7450cb8b15cc00e0d36c07bf55582
Yonas Mekonnen <myonas886@gmail.com>
2025-08-11 15:22:40 +0300

    solved linked list cycle problem if the linked list has a cycle in it.

--- linked_list/singly_linked_list/problems.py
+++ linked_list/singly_linked_list/problems.py
@@ -154,6 +154,16 @@ class LinkedList:
             fast = fast.next.next
         return slow
 
+    def has_loop(self):
+        slow = self.head
+        fast = self.head
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+
 
 my_linked_list = LinkedList(1)
 my_linked_list.append(2)
