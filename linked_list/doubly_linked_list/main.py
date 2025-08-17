51e7d901d79b5d991e2aee668d486e8d2c96b1a6
Yonas Mekonnen <myonas886@gmail.com>
2025-08-17 22:45:08 +0300

    prepend method implemented in doubly linked list

--- linked_list/doubly_linked_list/main.py
+++ linked_list/doubly_linked_list/main.py
@@ -49,24 +49,68 @@ class DoublyLinkedList:
         self.length -= 1
         return temp
 
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            self.length = 1
+        else:
+            self.head.prev = new_node
+            new_node.next = self.head
+            new_node.prev = None
+            self.head = new_node
+        self.length += 1
+        return True
 
-my_doubly_linked_list = DoublyLinkedList(1)
-my_doubly_linked_list.append(2)
+
+my_doubly_linked_list = DoublyLinkedList(2)
+my_doubly_linked_list.append(3)
+
+print("Before prepend():")
+print("----------------")
+print("Head:", my_doubly_linked_list.head.value)
+print("Tail:", my_doubly_linked_list.tail.value)
+print("Length:", my_doubly_linked_list.length, "\n")
+print("Doubly Linked List:")
+my_doubly_linked_list.print_list()
 
 
-# (2) Items - Returns 2 Node
-print(my_doubly_linked_list.pop().value)
-# (1) Item -  Returns 1 Node
-print(my_doubly_linked_list.pop().value)
-# (0) Items - Returns None
-print(my_doubly_linked_list.pop())
+my_doubly_linked_list.prepend(1)
+
+
+print("\n\nAfter prepend():")
+print("---------------")
+print("Head:", my_doubly_linked_list.head.value)
+print("Tail:", my_doubly_linked_list.tail.value)
+print("Length:", my_doubly_linked_list.length, "\n")
+print("Doubly Linked List:")
+my_doubly_linked_list.print_list()
 
 
 """
     EXPECTED OUTPUT:
+    
+    Before prepend():
     ----------------
+    Head: 2
+    Tail: 3
+    Length: 2 
+
+    Doubly Linked List:
     2
+    3
+
+
+    After prepend():
+    ---------------
+    Head: 1
+    Tail: 3
+    Length: 3 
+
+    Doubly Linked List:
     1
-    None
+    2
+    3
 
 """
