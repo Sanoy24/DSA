405c647917085b632ea025b4bed107036d56dd39
Yonas Mekonnen <myonas886@gmail.com>
2025-08-17 21:52:17 +0300

    print list for doubly linked list implemented

--- linked_list/doubly_linked_list/main.py
+++ linked_list/doubly_linked_list/main.py
@@ -11,3 +11,13 @@ class DoublyLinkedList:
         self.head = new_node
         self.tail = new_node
         self.length = 1
+
+    def print_list(self):
+        if self.head is None:
+            return None
+        else:
+            current = self.head
+            while current:
+                print(current.value, end=" ")
+                current = current.next
+            print()
