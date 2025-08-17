cc6eb4295d7c9da6523571a60dcfc81f0e4f188b
Yonas Mekonnen <myonas886@gmail.com>
2025-08-17 22:03:46 +0300

    implemented append for doubly linked list

--- linked_list/doubly_linked_list/main.py
+++ linked_list/doubly_linked_list/main.py
@@ -18,6 +18,44 @@ class DoublyLinkedList:
         else:
             current = self.head
             while current:
-                print(current.value, end=" ")
+                print(current.value)
                 current = current.next
             print()
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            self.length = 1
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+
+
+my_doubly_linked_list = DoublyLinkedList(1)
+my_doubly_linked_list.append(2)
+
+
+print("Head:", my_doubly_linked_list.head.value)
+print("Tail:", my_doubly_linked_list.tail.value)
+print("Length:", my_doubly_linked_list.length, "\n")
+
+print("Doubly Linked List:")
+my_doubly_linked_list.print_list()
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Head: 1
+    Tail: 2
+    Length: 2 
+
+    Doubly Linked List:
+    1
+    2
+    
+"""
