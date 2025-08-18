2438f1d20cbdef83acbc73cc99d11d8496560ae3
Yonas Mekonnen <myonas886@gmail.com>
2025-08-18 15:11:04 +0300

    doubly linked list reverse implemented

---
+++ linked_list/doubly_linked_list/problems.py
@@ -0,0 +1,190 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        if self.head is None:
+            return None
+        else:
+            current = self.head
+            while current:
+                print(current.value)
+                current = current.next
+            print()
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            self.length = 1
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def pop(self):
+        if self.head is None:
+            return None
+        temp = self.tail
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            temp.prev = None
+        self.length -= 1
+        return temp
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            self.length = 1
+        else:
+            self.head.prev = new_node
+            new_node.next = self.head
+            new_node.prev = None
+            self.head = new_node
+        self.length += 1
+        return True
+
+    def pop_first(self):
+        if self.head is None:
+            return None
+        temp = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+            temp.next = None
+        self.length -= 1
+        return temp
+
+    def get(self, index):
+        if index < 0 or index > self.length:
+            return None
+        temp = self.head
+        if index < self.length / 2:
+            for _ in range(index):
+                temp = temp.next
+            return temp
+        else:
+            temp = self.tail
+            for _ in range(self.length - 1, index, -1):
+                temp = temp.prev
+            return temp
+
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+
+    def insert(self, index: int, value: int):
+        if index < 0 or index > self.length:
+            return None
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        if self.head is None:  # Empty list case
+            self.head = new_node
+            self.tail = new_node
+            self.length += 1
+            return True
+        current = self.get(index)
+        prev = current.prev
+        new_node.next = current
+        new_node.prev = prev
+        prev.next = new_node
+        current.prev = new_node
+        self.length += 1
+        return True
+
+    def remove(self, index):
+        if index < 0 or index > self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        temp = self.get(index)
+        if temp is None:  # Safety check for get method
+            return None
+        prev = temp.prev
+        next_node = temp.next
+        prev.next = next_node
+        next_node.prev = prev
+        temp.next = None
+        temp.prev = None
+        self.length -= 1
+        return temp
+
+    def swap_first_last(self):
+        if self.head is None or self.head == self.tail:
+            return
+        self.head.value, self.tail.value = self.tail.value, self.head.value
+
+    def reverse(self):
+        current = self.head
+        while current is not None:
+            current.prev, current.next = current.next, current.prev
+            current = current.prev
+        self.head, self.tail = self.tail, self.head
+
+
+my_doubly_linked_list = DoublyLinkedList(1)
+my_doubly_linked_list.append(2)
+my_doubly_linked_list.append(3)
+my_doubly_linked_list.append(4)
+my_doubly_linked_list.append(5)
+
+
+print("DLL before reverse():")
+my_doubly_linked_list.print_list()
+
+
+my_doubly_linked_list.reverse()
+
+
+print("\nDLL after reverse():")
+my_doubly_linked_list.print_list()
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    DLL before reverse():
+    1
+    2
+    3
+    4
+    5
+
+    DLL after reverse():
+    5
+    4
+    3
+    2
+    1
+
+"""
