2addce6fe1c94cfe0725549fa91d88176cb1dc56
Yonas Mekonnen <myonas886@gmail.com>
2025-08-30 20:16:35 +0300

    subarray sum equal k problem solved

--- hash_table/problems.py
+++ hash_table/problems.py
@@ -319,3 +319,29 @@ class Solution:
 
 sol = Solution()
 print(sol.findIntersectionValues([4, 3, 2, 3, 1], [2, 2, 5, 2, 3, 6]))
+
+"""
+Subarray Sum Equals K
+Given an array of integers nums and an integer k, return the
+total number of subarrays whose sum equals to k.
+A subarray is a contiguous non-empty sequence of elements within an array.
+Example 1:
+Input: nums = [1,1,1], k = 2
+Output: 2
+Example 2:
+Input: nums = [1,2,3], k = 3
+Output: 2
+"""
+
+
+class Solution:
+    def subarraySum(self, nums: list[int], k: int) -> int:
+        prefix_sums = {0: 1}
+        current_sum = 0
+        count = 0
+        for num in nums:
+            current_sum += num
+            if (current_sum - k) in prefix_sums:
+                count += prefix_sums[current_sum - k]
+            prefix_sums[current_sum] = prefix_sums.get(current_sum, 0) + 1
+        return count
