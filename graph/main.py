7799d1264009671cfb3db7c3cb80a9c1b05e13c5
Yonas Mekonnen <myonas886@gmail.com>
2025-09-01 23:33:42 +0300

    graph class

---
+++ graph/main.py
@@ -0,0 +1,28 @@
+class Graph:
+    def __init__(self):
+        self.adj_list = {}
+
+    def add_vertex(self, vertex):
+        if vertex not in self.adj_list.keys():
+            self.adj_list[vertex] = []
+            return True
+        return False
+
+    def print_graph(self):
+        for vertex in self.adj_list:
+            print(vertex, " : ", self.adj_list.keys())
+
+
+myGraoh = Graph()
+myGraoh.add_vertex("A")
+
+print(myGraoh.print_graph())
+
+
+# {
+#     "A": ["C", "B"],
+#     "B": [],
+#     "C": [],
+#     "D": [],
+#     "E": [],
+# }
