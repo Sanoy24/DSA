6258af21cad15a2b7ac544b2c006a531bf17521b
Yonas Mekonnen <myonas886@gmail.com>
2025-09-04 23:47:19 +0300

    add edges function added

--- graph/main.py
+++ graph/main.py
@@ -1,28 +1,34 @@
-class Graph:
-    def __init__(self):
-        self.adj_list = {}
+from typing import Dict, List, Union
 
-    def add_vertex(self, vertex):
-        if vertex not in self.adj_list.keys():
+Vertex = Union[str, int]  # can store either strings or ints
+
+
+class Graph:
+    def __init__(self) -> None:
+        self.adj_list: Dict[Vertex, List[Vertex]] = {}
+
+    def add_vertex(self, vertex: Vertex) -> bool:
+        if vertex not in self.adj_list:
             self.adj_list[vertex] = []
             return True
         return False
 
-    def print_graph(self):
-        for vertex in self.adj_list:
-            print(vertex, " : ", self.adj_list.keys())
+    def print_graph(self) -> None:
+        for vertex, edges in self.adj_list.items():
+            print(vertex, " : ", edges)
+
+    def add_edges(self, v1: Vertex, v2: Vertex) -> bool:
+        if v1 in self.adj_list and v2 in self.adj_list:
+            self.adj_list[v1].append(v2)
+            self.adj_list[v2].append(v1)
+            return True
+        return False
 
 
-myGraoh = Graph()
-myGraoh.add_vertex("A")
+# Example usage
+myGraph = Graph()
+myGraph.add_vertex("A")
+myGraph.add_vertex("B")
+myGraph.add_edges("A", "B")
 
-print(myGraoh.print_graph())
-
-
-# {
-#     "A": ["C", "B"],
-#     "B": [],
-#     "C": [],
-#     "D": [],
-#     "E": [],
-# }
+myGraph.print_graph()
